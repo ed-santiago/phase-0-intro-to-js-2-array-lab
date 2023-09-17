8ad77f877a6f20ab41f9fc5ef8645523d130094d
Edmond <edmond.santiago7@gmail.com>
2023-09-17 17:15:29 +1000

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+  return cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+  return cats.shift(name);
+}
+
+function appendCat(copyOfCats) {
+  copyOfCats = cats.slice();
+  copyOfCats.push("Broom");
+  return copyOfCats;
+}
+
+function prependCat(copyOfCats) {
+  copyOfCats = cats.slice();
+  copyOfCats.unshift("Arnold");
+  return copyOfCats;
+}
+
+function removeLastCat(copyOfCats) {
+  copyOfCats = [...cats];
+  copyOfCats.pop();
+  return copyOfCats;
+}
+
+function removeFirstCat(copyOfCats) {
+  copyOfCats = [...cats];
+  copyOfCats.shift();
+  return copyOfCats;
+}
